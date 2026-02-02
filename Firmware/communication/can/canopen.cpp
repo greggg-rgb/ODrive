dc63017be93f65791e7106ff18a1635b4739162f
Loi Trinh <k1ase@yahoo.com.vn>
2026-02-02 13:04:54 +0000

    canopen: Do not process controlword when the motor is not ready yet, most likely in the startup sequence of self calibration step.

--- Firmware/communication/can/canopen.cpp
+++ Firmware/communication/can/canopen.cpp
@@ -670,6 +670,11 @@ void CANopen::process_controlword(co_unsigned16_t controlword) {
     // Store for next edge detection
     last_controlword_ = controlword;
 
+    if (axis.requested_state_ != Axis::AXIS_STATE_IDLE && axis.current_state_ != Axis::AXIS_STATE_CLOSED_LOOP_CONTROL) {
+        // The motor is most likely not ready to be enabled and switched on
+        return;
+    }
+
     // State machine (bits 0-3)
     uint8_t cmd = controlword & 0x000F;
 
